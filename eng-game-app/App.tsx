cfe332825ddcd3c01d2c0e8a30438fd4df3fd872
ozge <ozgecaylioglu@gmail.com>
2023-09-28 20:54:44 -0400

    added modal

--- eng-game-app/App.tsx
+++ eng-game-app/App.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from 'react';
-import { SafeAreaView, Pressable, Text, View } from 'react-native';
+import { SafeAreaView, Button, Text, View, ScrollView, Modal, TextInput } from 'react-native';
 import SearchBar from './src/components/SearchBar';
 
 export default function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <SafeAreaView className="h-screen m-3">
+    <SafeAreaView className="h-screen">
       <View>
-        <SearchBar />
-        {/* latestSavedWords */}
+        <View>
+          <SearchBar />
+        </View>
+        <View>
+          <Button title="Add New" onPress={() => setOpenModal(true)} />
+        </View>
+        <View>
+          <Modal visible={openModal} animationType="slide">
+            <View>
+              <Text>Hello</Text>
+              <TextInput />
+            </View>
+            <View>
+              <Button title="Cancel" onPress={() => setOpenModal(false)} />
+            </View>
+          </Modal>
+        </View>
+
+        {/* latestSavedWords 5e0acc*/}
       </View>
     </SafeAreaView>
   );
--- eng-game-app/src/components/SearchBar.tsx
+++ eng-game-app/src/components/SearchBar.tsx
@@ -15,7 +15,7 @@ export default function SearchBar() {
         className="bg-slate-200 w-80 h-10 border-solid border-r-violet-800 rounded-md mx-auto mt-4 pl-4"
         placeholder="search"
         value={searchText}
-        onChangeText={(e) => console.log('e', e)}
+        onChangeText={(e) => setSearchText(e)}
       />
     </View>
   );
