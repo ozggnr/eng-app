f27d24c2f54599949130e1e8e2b24121874a3710
ozge <ozgecaylioglu@gmail.com>
2023-09-28 20:54:58 -0400

    added word and document types

--- eng-game-app/Types.ts
+++ eng-game-app/Types.ts
@@ -1,4 +1,14 @@
 export type Document = {
-   id: string;
-   lines: String[]
-}
+  id: string;
+  lines: String[];
+  date: string;
+};
+
+export type Word = {
+  id: string;
+  word: string;
+  cdefinition: string;
+  ddefinition: string;
+  synonyms: Word[];
+  example: string;
+};
